96b3940bb25cb58b3d40f935e3d84cdef9abee25
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-12-16 13:37:19 +0530

    add all require apis which is need to be added

---
+++ pages/api/updateTodo.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { TodoModal } from "../../models";
+
+export default async function handler(req, res) {
+    if (req.method === "PUT") {
+        await mongoose.connect(process.env.DB_URL)
+            .then((value) => {
+                console.log('mongoDb connected');
+            })
+            .catch((err) => {
+                console.log('err');
+            });
+        await TodoModal.updateOne({_id: req.body.itemId}).then(() => {
+            res.status(200).end();
+        }).catch(() => {
+            res.status(404).end();
+        }).finally(async () => {
+            await mongoose.disconnect();
+        })
+    }
+}
